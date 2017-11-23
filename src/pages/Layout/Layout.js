becdf2453233a71f6dff3f065dfbaa27b82eebfd
Paulin Trognon <paulin.trognon@gmail.com>
2017-11-23 10:48:58 +0100

    match style to base app

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,23 +1,41 @@
 import React from 'react';
 
-import './reset.css';
+import './base.css';
 import './layout.css';
+
+import config from 'config';
+
+const forumLink = `${config.laravelHost}/forum`;
+const logoutLink = `${config.laravelHost}/logout`;
 
 export default class Layout extends React.Component {
   render() {
     return (
-      <div className="full-container">
+      <div>
         <nav className="main-nav navbar navbar-dark">
           <div className="container">
             <a className="navbar-brand" href="/">
-              Armies At War
+            Armies At War
+          </a>
+          <ul class="nav justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link active" href="/">Jouer</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href={forumLink}>Forum</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href={logoutLink}>
+              Déconnection
             </a>
-          </div>
-        </nav>
-        <div className="container main-content">
-          {this.props.children}
-        </div>
-      </div>
-    )
-  }
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div className="container main-content">
+    {this.props.children}
+  </div>
+</div>
+)
+}
 }
